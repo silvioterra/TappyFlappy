7dabf47ae612cd1d26dc1a4d9041d4cb76f345c1
silvioterra <github@multiveritas.com>
2016-01-24 23:06:21 -0800

    Turrets now are slightly phallic.
    Added a non-death ceiling. Unknown WHERE the real ceiling is in TB, but this seems OK.
    Turrets now have an extended collider for birds out of bounds

---
+++ TappyFlappy/Assets/Src/Obstacle.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Obstacle : MonoBehaviour, IScreenListener, ISpeedListener
+{
+
+    public const float MaxHeightScreenPercent = 0.33f;
+
+    protected bool mInUse;
+    protected Vector2 mPositionInWorld;
+    protected float mWorldHeight;
+    protected float mWidth = 2.0f;
+
+    public float Width
+    {
+        get { return mWidth; }
+        set { mWidth = value; }
+    }
+
+    public float WorldPosition
+    {
+        get { return mPositionInWorld.x; }
+        set { SetWorldPosition(value); }
+    }
+
+    public bool Used
+    {
+        get
+        {
+            return mInUse;
+        }
+
+        set
+        {
+            SetUsed(value);
+        }
+    }
+
+    void IScreenListener.OnScreenResolutionChanged(int width, int height)
+    {
+        UpdateMaxHeight();
+    }
+
+    virtual protected void UpdateMaxHeight()
+    {
+        Vector3 top = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 bottom = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0));
+        mWorldHeight = (bottom - top).y;
+
+        SetWorldPosition(mPositionInWorld.x);
+    }
+
+
+    public void Hide()
+    {
+        // Just moving it out of the way for now...
+        this.transform.position = new Vector3(-20, -20, 0);
+        //enabled = false;
+    }
+
+    virtual protected void InternalAwake()
+    {
+        mPositionInWorld = Vector2.zero;
+    }
+    void Awake()
+    {
+        InternalAwake();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Game.GetInstance().AddScreenListener(this);
+        Game.GetInstance().GetLevelManager().AddSpeedListener(this);
+        UpdateMaxHeight();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void SetUsed(bool used)
+    {
+        if (mInUse == used)
+        {
+            return;
+        }
+        mInUse = used;
+        //enabled = used;
+        if (!used)
+        {
+            Hide();
+        }
+        else
+        {
+            UpdateMaxHeight();
+        }
+    }
+
+    virtual protected void SetWorldPosition(float xPosition)
+    {
+        // This really probably shouldn't change once set
+        mPositionInWorld = new Vector2(xPosition, 1.0f);
+    }
+
+    void ISpeedListener.OnScreenCenterPositionUpdated(float worldPosition)
+    {
+        if (mInUse)
+        {
+            // Determine the new screen position for this turret
+
+            Vector3 left = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+            Vector3 right = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
+            Vector3 worldWidth = right - left;
+
+            // This is how far we are from the center of the screen, in world units
+            float xPos = mPositionInWorld.x - worldPosition;
+            Vector3 placement = Camera.main.ViewportToWorldPoint(new Vector3(xPos, 0, 0));
+            // Hack. Without this the 33% starts from the BOTTOM of the screen. The Ground is at 1.0f
+            // So this allows for a small amount of turret penetration
+            const float kGroundHeight = 0.9f;
+            this.transform.position = new Vector3(xPos, -Camera.main.orthographicSize + mPositionInWorld.y / 2.0f + kGroundHeight, 0);
+            //-Camera.main.orthographicSize + (newObstacle.mHeight)
+        }
+
+    }
+
+    void ISpeedListener.OnSpeedChanged(float newSpeed)
+    {
+        //throw new NotImplementedException();
+    }
+}
--- TappyFlappy/Assets/Src/Turret.cs
+++ TappyFlappy/Assets/Src/Turret.cs
@@ -2,154 +2,56 @@
 using System.Collections;
 using System;
 
-public class Turret : MonoBehaviour, IScreenListener, ISpeedListener
+public class Turret : Obstacle
 {
-
-    public const float MaxHeightScreenPercent = 0.33f;
-
-    float mMaxHeight;
+    private float mTurretHeight = 1.0f;
+    private BoxCollider2D mHackCollider;
     GameObject mShaft;
     private BoxCollider2D mShaftCollider;
-    private float mTurretHeight = 1.0f;
-    private float mTurretWidth = 2.0f;
-    private bool mInUse;
-    private Vector2 mPositionInWorld;
-    private float mWorldHeight;
+    float mMaxHeight;
+    private GameObject mGun;
 
-    public float Width
+    protected override void UpdateMaxHeight()
     {
-        get { return mTurretWidth; }
-        set { mTurretWidth = value; }
-    }
-
-    public float WorldPosition
-    {
-        get { return mPositionInWorld.x; }
-        set { SetWorldPosition(value); }
-    }
-
-    public bool Used
-    {
-        get
-        {
-            return mInUse;
-        }
-
-        set
-        {
-            SetUsed(value);
-        }
-    }
-
-    void IScreenListener.OnScreenResolutionChanged(int width, int height)
-    {
-        UpdateMaxHeight();
-    }
-
-    private void UpdateMaxHeight()
-    {
-        Vector3 top = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
-        Vector3 bottom = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0));
-        mWorldHeight = (bottom - top).y;
-
-        mMaxHeight = MaxHeightScreenPercent;// Camera.main.orthographicSize * 2.0f;
+        base.UpdateMaxHeight();
         SetHeight(mTurretHeight);
-        SetWorldPosition(mPositionInWorld.x);
 
     }
+
 
     public void SetHeight(float height)
     {
         mTurretHeight = Math.Min(height, mMaxHeight) * mWorldHeight;
 
         // Adjust Shaft scale
-        mShaft.transform.localScale = new Vector3(mTurretWidth, mTurretHeight, 0.0f);
+        mShaft.transform.localScale = new Vector3(mWidth, mTurretHeight, 0.0f);
         // Update the tiling for the turret
         mShaft.GetComponent<Renderer>().material.mainTextureScale = new Vector2(1.0f, mTurretHeight / 2.0f);
         // TODO - remove the actual gun height from this height. 
 
+        mHackCollider.size = new Vector2(mWidth, 1.0f);
+        mGun.transform.localScale = new Vector3(mWidth, 1.0f, 1.0f);
+        // Assuming mGun's scale is always 1.0, not going to change this.
+        mGun.transform.localPosition = new Vector3(0, mTurretHeight / 2.0f + 0.5f, 0);
         // Adjust the box collider and the visible elements
-        mShaftCollider.size = new Vector2(mTurretWidth, mTurretHeight);
+        mShaftCollider.size = new Vector2(mWidth, mTurretHeight);
 
     }
-
-    public void Hide()
+    protected override void SetWorldPosition(float xPosition)
     {
-        // Just moving it out of the way for now...
-        this.transform.position = new Vector3(-20, -20, 0);
-        //enabled = false;
-    }
-
-    void Awake()
-    {
-        mShaft = this.transform.Find("Shaft").gameObject;
-        mShaftCollider = GetComponent<BoxCollider2D>();
-        mPositionInWorld = Vector2.zero;
-    }
-
-    // Use this for initialization
-    void Start ()
-    {
-        Game.GetInstance().AddScreenListener(this);
-        Game.GetInstance().GetLevelManager().AddSpeedListener(this);
-        UpdateMaxHeight();
-    }
-
-    // Update is called once per frame
-    void Update ()
-    {
-	    
-	}
-
-   
-    public void SetUsed(bool used)
-    {
-        if (mInUse == used)
-        {
-            return;
-        }
-        mInUse = used;
-        //enabled = used;
-        if (!used)
-        {
-            Hide();
-        }
-        else
-        {
-            UpdateMaxHeight();
-        }
-    }
-
-    private void SetWorldPosition(float xPosition)
-    {
-        // This really probably shouldn't change once set
         mPositionInWorld = new Vector2(xPosition, mTurretHeight);
     }
 
-    void ISpeedListener.OnScreenCenterPositionUpdated(float worldPosition)
+    protected override void InternalAwake()
     {
-        if (mInUse)
-        {
-            // Determine the new screen position for this turret
+        base.InternalAwake();
+        mMaxHeight = MaxHeightScreenPercent;// Camera.main.orthographicSize * 2.0f;
 
-            Vector3 left = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
-            Vector3 right = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
-            Vector3 worldWidth = right - left;
+        mShaft = this.transform.Find("Shaft").gameObject;
+        mGun = this.transform.Find("Gun").gameObject;
+        mHackCollider = this.transform.Find("Shaft/InfinityTube").gameObject.GetComponent<BoxCollider2D>();
+        
+        mShaftCollider = GetComponent<BoxCollider2D>();
 
-            // This is how far we are from the center of the screen, in world units
-            float xPos = mPositionInWorld.x - worldPosition;
-            Vector3 placement = Camera.main.ViewportToWorldPoint(new Vector3(xPos, 0, 0));
-            // Hack. Without this the 33% starts from the BOTTOM of the screen. The Ground is at 1.0f
-            // So this allows for a small amount of turret penetration
-            const float kGroundHeight = 0.9f; 
-            this.transform.position = new Vector3(xPos, -Camera.main.orthographicSize + mPositionInWorld.y / 2.0f + kGroundHeight, 0);
-            //-Camera.main.orthographicSize + (newObstacle.mHeight)
-        }
-
-    }
-
-    void ISpeedListener.OnSpeedChanged(float newSpeed)
-    {
-        //throw new NotImplementedException();
     }
 }
